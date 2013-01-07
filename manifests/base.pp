7b1e761402918a33e604d1596ef61651de7150d5
Eric Rochester <erochest@virginia.edu>
2013-01-07 14:22:09 -0500

    More infrastructure for Omeka.

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,8 +1,43 @@
-package { 'nginx':
-  ensure => installed,
+## {{{ Execs
+
+exec { 'apt-get update':
+  path => ['/usr/bin'],
 }
+
+#}}}
+
+## {{{ Packages
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get update'],
+}
+
+package { 'mysql-server':
+  ensure  => installed,
+  require => Exec['apt-get update'],
+}
+
+package { 'imagemagick':
+  ensure  => installed,
+  require => Exec['apt-get update'],
+}
+
+# }}}
+
+## {{{ Services
 
 service { 'nginx':
   ensure  => running,
-  enabled => true,
+  enable  => true,
+  require => Package['nginx'],
 }
+
+service { 'mysql':
+  ensure  => running,
+  enable  => true,
+  require => Package['mysql-server'],
+}
+
+# }}}
+# vim :set foldmethod=marker:
