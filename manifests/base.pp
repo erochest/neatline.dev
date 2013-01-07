b568cfe6954035c7e15f63ff135fbe2f8531782c
Eric Rochester <erochest@virginia.edu>
2013-01-07 15:08:37 -0500

    Added initial attempt to install Omeka.

--- manifests/base.pp
+++ manifests/base.pp
@@ -23,6 +23,11 @@ package { 'imagemagick':
   require => Exec['apt-get update'],
 }
 
+package { 'git':
+  ensure => installed,
+  require => Exec['apt-get update'],
+}
+
 # }}}
 
 ## {{{ Services
@@ -40,4 +45,25 @@ service { 'mysql':
 }
 
 # }}}
+
+## {{{ Omeka
+
+exec { 'omeka':
+  cwd     => '/vagrant',
+  command => 'git clone --branch stable-1.5 git://github.com/omeka/Omeka.git && git submodule init && git submodule update',
+  path    => ['/usr/bin/'],
+  require => Package['git'],
+  creates => '/vagrant/Omeka',
+}
+
+exec { 'neatline-time':
+  cwd     => '/vagrant/Omeka/plugins',
+  command => 'git clone git://github.com/scholarslab/NeatlineTime.git',
+  path    => ['/usr/bin/'],
+  require => [Exec['omeka'], Package['git']],
+  creates => '/vagrant/Omeka/plugins/NeatlineTime',
+}
+
+# }}}
+
 # vim :set foldmethod=marker:
