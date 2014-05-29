4fa86bc36f84d1c258aab1757cbaea659d5cad8e
Eric Rochester <erochest@virginia.edu>
2014-05-29 15:21:28 -0400

    Install Omeka from github.

--- scripts/vhost.py
+++ scripts/vhost.py
@@ -6,8 +6,21 @@ import sys
 
 VHOST = '''
 <VirtualHost *:80>
-    ServerName {}
-    DocumentRoot "{}"
+    ServerName {hostname}
+    DocumentRoot "{dirname}"
+    <Directory />
+        Options FollowSymLinks
+        AllowOverride All
+    </Directory>
+    <Directory {dirname}>
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride All
+        Order allow,deny
+        Allow from all
+    </Directory>
+
+    ErrorLog /private/var/log/apache2/{hostname}_error.log
+    CustomLog /private/var/log/apache2/{hostname}_access.log combined
 </VirtualHost>
 '''
 
@@ -20,7 +33,7 @@ def main():
 
     if hostname not in config or dirname not in config:
         with open(vhost_file, 'a') as f:
-            f.write(VHOST.format(hostname, dirname))
+            f.write(VHOST.format(hostname=hostname, dirname=dirname))
 
 
 if __name__ == '__main__':
